6c7d2e3b2ccaf5949c63775b6499f5303f7ce09e
Bdwrds <edwards.benjamin.james@gmail.com>
2021-12-22 08:41:02 +0000

    write api results to file

--- apicalls.py
+++ apicalls.py
@@ -1,4 +1,12 @@
 import requests
+import json
+import os
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+fp_output_path = config['output_model_path']
+fp_api_res = os.path.join(fp_output_path, 'apireturns.txt')
 
 #Specify a URL that resolves to your workspace
 URL = "http://127.0.0.1:"
@@ -13,13 +21,15 @@ response4 = requests.get(URL + str(PORT) + "/diagnostics" + QUERY).content
 
 #combine all API responses
 responses = {
-    "prediction": response1,
-    "scoring": response2,
-    "summarystats": response3,
-    "diagnostics": response4,
+    "prediction": response1.decode('utf-8'),
+    "scoring": response2.decode('utf-8'),
+    "summarystats": response3.decode('utf-8'),
+    "diagnostics": response4.decode('utf-8'),
              }
 
 #write the responses to your workspace
 print(responses)
 
+with open(fp_api_res, 'w') as api_res:
+    json.dump(responses, api_res)
 
